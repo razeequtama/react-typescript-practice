bae7bca5345391964b2f55847c9c79b27361e92c
Raziq <rabbaniu74m4@gmail.com>
2026-08-22 03:40:12 +0700

    Initialize: Project 6 second attempt from scratch

--- 06-habit-tracker-v2/src/App.tsx
+++ 06-habit-tracker-v2/src/App.tsx
@@ -1,17 +1,6 @@
-import { DateContextProvider } from "./context/DateContext";
-import { HabitContextProvider } from "./context/HabitContext";
-import Header from "./component/Header";
-import HabitEntryListSet from "./component/HabitEntryListSet";
-import "./App.css"
-
 export default function App()
 {
   return(
-    <HabitContextProvider>
-    <DateContextProvider>
-      <Header />
-      <HabitEntryListSet/>
-    </DateContextProvider>
-    </HabitContextProvider>
+    <h1>Hi</h1>
   )
 }
